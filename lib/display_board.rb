550fbee4c03559fd29b771171a95715c1afc018f
Nicole Wachter <nicole.wachter6@gmail.com>
2021-01-27 18:40:14 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,7 +4,7 @@ def display_board(board)
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
-end 
+end
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 # Define display_board that accepts a board and prints
