750f9b0078003a0ee02e3807941f8db48d1d053f
Nicole Wachter <nicole.wachter6@gmail.com>
2021-01-27 18:40:10 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,11 @@
+def display_board(board)
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end 
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
 # Define display_board that accepts a board and prints
 # out the current state.
