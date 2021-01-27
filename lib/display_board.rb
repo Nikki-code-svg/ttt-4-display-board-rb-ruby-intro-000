8e39b9179b106b499ddf1226b9bece8020fc0cd2
Nicole Wachter <nicole.wachter6@gmail.com>
2021-01-27 19:06:25 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -6,6 +6,17 @@ def display_board(board)
   puts "   |   |   "
 end
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+display_board(board)
+board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
+display_board(board)
+board = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
+display_board(board)
+board = ["O", "X", " ", " ", "X", " ", "X", "O", " "]
+display_board(board)
+board = ["O", "O", "O", "O", "O", "O", "O", "O", "O"]
+display_board(board)
+board = ["O", "X", " ", " ", "X", " ", "X", "O", " "]
+display_board(board)
 
 # Define display_board that accepts a board and prints
 # out the current state.
